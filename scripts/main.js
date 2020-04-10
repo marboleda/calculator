479014192ab6b0a484b61f4b26bd374330a8f1c3
Marco <arboleda.marco@gmail.com>
2020-04-10 11:42:40 -0400

    Create operate function

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,22 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operation, num1, num2) {
+    switch(operation) {
+        case "add":
+            return add(num1,num2);
+            break;
+        case "subtract":
+            return subtract(num1,num2);
+            break;
+        case "multiply":
+            return multiply(num1,num2);
+            break;
+        case "divide":
+            return divide(num1,num2);
+            break;
+        default:
+            console.log("Invalid operation");
+    }
+}
