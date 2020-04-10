4523f4f782059cca4e498892e0cee445e04f5e37
Marco <arboleda.marco@gmail.com>
2020-04-10 19:29:00 -0400

    Implement order of operations logic

--- scripts/main.js
+++ scripts/main.js
@@ -33,6 +33,27 @@ function operate(operation, num1, num2) {
     }
 }
 
+function equalsFunctionality(numsAndOperators) {
+    let result;
+    let indexOfOperatorWithHighestPrecedence = 0;
+    let currentPrecedenceValue = 0;
+    while (numsAndOperators.length != 0) {
+        for (let i = 0; i < numsAndOperators.length; i++) {
+            if (numsAndOperators[i] == "x" || numsAndOperators[i] == "÷") {
+                if (currentPrecedenceValue < 14) {
+                    currentPrecedenceValue = 14;
+                    indexOfOperatorWithHighestPrecedence = i;
+                }
+            } else {
+                if (currentPrecedenceValue < 13) {
+                    currentPrecedneceValue = 13;
+                    indexOfOperatorWithHighestPrecedence = i;
+                }
+            }
+        }
+    }
+}
+
 let displayValue = "";
 let currentNumDisplayValue = "";
 let values = [];
@@ -49,9 +70,12 @@ numButtons.forEach((button) => {
             values.push(button.textContent);
             currentNumDisplayValue = "";
             displayValue += " " + button.textContent + " ";
+        } else if (button.id == "equals-button") {
+
         } else {
             displayValue = "";
         }
         document.querySelector("#display").textContent = displayValue;
     });
 });
+
