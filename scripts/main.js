0c1f51b0173881d97b497da434dc23bfdf19f84e
Marco <arboleda.marco@gmail.com>
2020-04-10 22:52:20 -0400

    Implement equal sign functionality

--- scripts/main.js
+++ scripts/main.js
@@ -16,42 +16,52 @@ function divide(num1, num2) {
 
 function operate(operation, num1, num2) {
     switch(operation) {
-        case "add":
+        case "+":
             return add(num1,num2);
             break;
-        case "subtract":
+        case "-":
             return subtract(num1,num2);
             break;
-        case "multiply":
+        case "x":
             return multiply(num1,num2);
             break;
-        case "divide":
+        case "÷":
             return divide(num1,num2);
             break;
         default:
             console.log("Invalid operation");
+            break;
     }
 }
 
 function equalsFunctionality(numsAndOperators) {
     let result;
-    let indexOfOperatorWithHighestPrecedence = 0;
+    let highestPrecedenceIndex = 0;
     let currentPrecedenceValue = 0;
-    while (numsAndOperators.length != 0) {
+    console.log(numsAndOperators);
+    while (numsAndOperators.length != 1) {
+
         for (let i = 0; i < numsAndOperators.length; i++) {
             if (numsAndOperators[i] == "x" || numsAndOperators[i] == "÷") {
                 if (currentPrecedenceValue < 14) {
                     currentPrecedenceValue = 14;
-                    indexOfOperatorWithHighestPrecedence = i;
+                    highestPrecedenceIndex = i;
                 }
             } else {
                 if (currentPrecedenceValue < 13) {
-                    currentPrecedneceValue = 13;
-                    indexOfOperatorWithHighestPrecedence = i;
+                    currentPrecedenceValue = 13;
+                    highestPrecedenceIndex = i;
                 }
             }
         }
+        result = operate(numsAndOperators[highestPrecedenceIndex],
+                         numsAndOperators[highestPrecedenceIndex-1],
+                         numsAndOperators[highestPrecedenceIndex+1]);
+            
+        values[highestPrecedenceIndex+1] = result;
+        values.splice(highestPrecedenceIndex-1, 2);
     }
+    return result;
 }
 
 let displayValue = "";
@@ -70,8 +80,12 @@ numButtons.forEach((button) => {
             values.push(button.textContent);
             currentNumDisplayValue = "";
             displayValue += " " + button.textContent + " ";
+            console.log(values);
         } else if (button.id == "equals-button") {
-
+            values.push(currentNumDisplayValue);
+            currentNumDisplayValue = equalsFunctionality(values);
+            values = [currentNumDisplayValue];
+            displayValue = currentNumDisplayValue;
         } else {
             displayValue = "";
         }
